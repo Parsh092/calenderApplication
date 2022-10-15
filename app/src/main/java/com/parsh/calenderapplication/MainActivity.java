95f4078ff30dce7a8208dbd3b542a508cd4523a7
Parshva Choradia <parshchoradia0902@gmail.com>
2022-10-15 20:44:30 +0530

    Added Firbase

--- app/src/main/java/com/parsh/calenderapplication/MainActivity.java
+++ app/src/main/java/com/parsh/calenderapplication/MainActivity.java
@@ -12,32 +12,33 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-CalendarView calendarView;
-TextView date;
-Button button;
+    CalendarView calendarView;
+    TextView date;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         calendarView = findViewById(R.id.calendarView);
-        date= findViewById(R.id.Date);
-button = findViewById(R.id.button);
-final String g="Enter Your Details";
+        date = findViewById(R.id.Date);
+        button = findViewById(R.id.button);
+        final String g = "Enter Your Details";
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String daate = "Date: "+dayOfMonth+"-"+((month)+1)+"-"+year;
-                String g=daate;
+                String daate = "Date: " + dayOfMonth + "-" + ((month) + 1) + "-" + year;
+                String g = daate;
                 date.setText(daate);
-                Toast.makeText(MainActivity.this,"You have Selected a date",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "You have Selected a date", Toast.LENGTH_SHORT).show();
             }
         });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,appointment.class);
-                intent.putExtra("value1",g);
+                Intent intent = new Intent(MainActivity.this, appointment.class);
+                intent.putExtra("value1", g);
                 startActivity(intent);
 
             }
