6fdb7a6de68b2a35dcd5c7a3b76b40e9a5cbb558
Parshva Choradia <parshchoradia0902@gmail.com>
2022-10-17 18:33:29 +0530

    Again,try same date should not be selected

--- app/src/main/java/com/parsh/calenderapplication/MainActivity.java
+++ app/src/main/java/com/parsh/calenderapplication/MainActivity.java
@@ -13,24 +13,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     CalendarView calendarView;
     TextView date;
     Button button;
-    FirebaseAuth auth;
+    FirebaseAuth firebaseAuth;
     FirebaseDatabase database;
     FirebaseStorage storage;
+    FirebaseFirestore firebaseFirestore;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +45,12 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         final String g = "Enter Your Details";
         Firebase firebase;
-        auth = FirebaseAuth.getInstance();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
         database = FirebaseDatabase.getInstance();
-        storage = FirebaseStorage.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
         Firebase.setAndroidContext(this);
         String UniqueId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
         firebase = new Firebase("https://calenderapplication-3035f-default-rtdb.firebaseio.com/Users" + UniqueId);
@@ -56,25 +63,45 @@ public class MainActivity extends AppCompatActivity {
                 compare = compare.replace("-", "");
                 date.setText(daate);
                 String finalCompare = compare;
-                FirebaseDatabase.getInstance().getReference().
-                        child("Date").equalTo(compare).addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                if (snapshot.child("Date").exists()){
-                                    Toast.makeText(MainActivity.this,"Date is already selected",Toast.LENGTH_SHORT).show();
-                                }
-                                else{
-                                    Firebase child_date = firebase.child("Date");
-                                    child_date.setValue(finalCompare);
-                                    Toast.makeText(MainActivity.this, "You have Selected a date", Toast.LENGTH_SHORT).show();
-                                }
-                            }
+//                firebaseFirestore.collection("Users").document(compare).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                        if (task.getResult().exists()){
+//                          Toast.makeText(MainActivity.this,"This date is already selected",Toast.LENGTH_SHORT).show();
+//                        }
+//                        else{
+//                            Firebase child_date = firebase.child("Date");
+//                                    child_date.setValue(finalCompare);
+//                                    Toast.makeText(MainActivity.this, "You have Selected a date", Toast.LENGTH_SHORT).show();
+//
+//                        }
+//                    }
+//                });
 
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
 
+                FirebaseDatabase.getInstance().getReference().equalTo(compare).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.child("Date").exists()) {
+                            Toast.makeText(MainActivity.this, "Date is already selected", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Firebase child_date = firebase.child("Date");
+                            if (child_date.getKey()== finalCompare) {
+                                Toast.makeText(MainActivity.this, "This date is already selected", Toast.LENGTH_SHORT).show();
+                            } else {
+                                child_date.setValue(finalCompare);
+                                Toast.makeText(MainActivity.this, "You have Selected a date", Toast.LENGTH_SHORT).show();
                             }
-                        });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
 //                Firebase child_date = firebase.child("Date");
 //                    child_date.setValue(compare);
 //                    Toast.makeText(MainActivity.this, "You have Selected a date", Toast.LENGTH_SHORT).show();
